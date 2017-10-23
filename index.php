f913699f5c8d9f179ef11995c7caf619d914f011
raj <241096raj@gmail.com>
2017-10-23 15:17:59 +0530

    First Commit : Basic File Upload using form

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AJAX Upload Service</title>
+</head>
+<body>
+<form name="upload-form" method="POST" action="upload.php" enctype="multipart/form-data">
+	<input type="file" name="fileToUpload" id="fileToUpload" />
+	<input type="submit" value="Upload" name="submit" />
+</form>
+
+</body>
+<script type="text/javascript" src="js/script.js"></script>
+</html>
