1bb95d8d11fc3e190d151e4c28d907b75e6fc35a
raj <241096raj@gmail.com>
2017-10-23 21:49:03 +0530

    AJAX Upload

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 	<title>AJAX Upload Service</title>
 </head>
 <body>
-<form name="upload-form" method="POST" action="upload.php" enctype="multipart/form-data">
+<form>
 	<input type="file" name="fileToUpload" id="fileToUpload" />
-	<input type="submit" value="Upload" name="submit" />
+	<input type="submit" value="Upload" name="submit" id="upload"/>
 </form>
-
+<div id="drop-box">
+	
+</div>
 </body>
+<script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/script.js"></script>
 </html>
