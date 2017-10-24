dbd978af5b39c053fbf6ce799baf7f2a43dc1c81
raj <241096raj@gmail.com>
2017-10-24 13:00:16 +0530

    AJAX Upload with specified file path and progress renderer

--- index.php
+++ index.php
@@ -4,13 +4,10 @@
 	<title>AJAX Upload Service</title>
 </head>
 <body>
-<form>
+<form id="uploadForm" >
 	<input type="file" name="fileToUpload" id="fileToUpload" />
 	<input type="submit" value="Upload" name="submit" id="upload"/>
 </form>
-<div id="drop-box">
-	
-</div>
 </body>
 <script type="text/javascript" src="js/jquery.min.js"></script>
 <script type="text/javascript" src="js/script.js"></script>
--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 $(document).ready(function(){
-	$("form").submit(function(evt){	 
-      evt.preventDefault();
-      var formData = new FormData($(this)[0]);
-   $.ajax({
-       url: 'upload.php',
-       type: 'POST',
-       data: formData,
-       async: false,
-       cache: false,
-       contentType: false,
-       enctype: 'multipart/form-data',
-       processData: false,
-       success: function (response) {
-         alert(response);
-       }
-   });
-   return false;
- });
+	$("form[id='uploadForm']").submit(function(evt){	 
+      	evt.preventDefault();
+      	var formData = new FormData($(this)[0]);
+      	formData.append('path','files/');
+   		$.ajax({
+       		url: 'upload.php',
+       		type: 'POST',
+       		data: formData,
+       		cache: false,
+       		contentType: false,
+       		enctype: 'multipart/form-data',
+       		processData: false,
+       		success: function (response) {
+        				alert(response);
+       				},
+       		beforeSend: function (xhr){
+       			xhr.upload.addEventListener('progress',renderProgresBar,false);
+       		}
+
+   		});
+   		return false;
+ 	});
+
+	function renderProgressBae(e){
+		if(e.lengthComputable){
+			var percent = e.loaded/e.total;
+			
+		}
+	}
+
 });
